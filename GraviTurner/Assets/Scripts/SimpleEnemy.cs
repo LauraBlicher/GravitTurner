cf86f136e2ebb6111598114c1f3e418ec81a0607
Laura Uhliarova <lau.uhl@gmail.com>
2019-01-14 11:57:38 +0100

    crystalize armour and new levels

---
+++ GraviTurner/Assets/Scripts/SimpleEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : MonoBehaviour {
+
+    float dirX;
+    public float moveSpeed = 3f;
+    bool moveRight = true;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 4f)
+            moveRight = false;
+        if (transform.position.x < -4f)
+            moveRight = true;
+
+        if (moveRight)
+            transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+    }
+
+
+}
---
+++ GraviTurner/Assets/Scripts/UpDownMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownMovement: MonoBehaviour {
+
+    public float delta = 1.5f;
+    public float speed = 2.0f;
+    private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+
+        startPos = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 v = startPos;
+        v.y += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+
+        }
+}
